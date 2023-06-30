d87475214a90500674f7453c5ceb967a49e5665c
maple <290204957@qq.com>
2023-06-30 21:37:12 +0900

    feat: add routers && utils

--- go.mod
+++ go.mod
@@ -2,14 +2,18 @@ module iris-myapp
 
 go 1.20
 
+require github.com/kataras/iris v0.0.2
+
 require (
 	github.com/BurntSushi/toml v1.2.1 // indirect
 	github.com/CloudyKit/fastprinter v0.0.0-20200109182630-33d98a066a53 // indirect
+	github.com/CloudyKit/jet/v4 v4.1.0 // indirect
 	github.com/CloudyKit/jet/v6 v6.2.0 // indirect
 	github.com/Joker/jade v1.1.3 // indirect
 	github.com/Shopify/goreferrer v0.0.0-20220729165902-8cddb4f5de06 // indirect
 	github.com/andybalholm/brotli v1.0.5 // indirect
 	github.com/aymerick/douceur v0.2.0 // indirect
+	github.com/aymerick/raymond v2.0.3-0.20180322193309-b565731e1464+incompatible // indirect
 	github.com/eknkc/amber v0.0.0-20171010120322-cdade1c07385 // indirect
 	github.com/fatih/structs v1.1.0 // indirect
 	github.com/flosch/pongo2/v4 v4.0.2 // indirect
@@ -17,8 +21,11 @@ require (
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/gorilla/css v1.0.0 // indirect
 	github.com/howeyc/fsnotify v0.9.0 // indirect
+	github.com/iris-contrib/jade v1.1.4 // indirect
+	github.com/iris-contrib/pongo2 v0.0.1 // indirect
 	github.com/iris-contrib/schema v0.0.6 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/kataras/blocks v0.0.7 // indirect
 	github.com/kataras/golog v0.1.8 // indirect
 	github.com/kataras/iris/v12 v12.2.0 // indirect
@@ -31,9 +38,12 @@ require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/microcosm-cc/bluemonday v1.0.23 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pilu/config v0.0.0-20131214182432-3eb99e6c0b9a // indirect
 	github.com/pilu/fresh v0.0.0-20190826141211-0fa698148017 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/ryanuber/columnize v2.1.0+incompatible // indirect
 	github.com/schollz/closestmatch v2.1.0+incompatible // indirect
 	github.com/sirupsen/logrus v1.8.1 // indirect
 	github.com/tdewolff/minify/v2 v2.12.4 // indirect
---
+++ routers/routers.go
@@ -0,0 +1,138 @@
+package routers
+
+import "github.com/kataras/iris/v12"
+
+type Response struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
+
+type SlidesProps struct {
+	Id    int    `json:"id"`
+	Image string `json:"image"`
+}
+
+type CategoryProps struct {
+	Id    int    `json:"id"`
+	Icon  string `json:"icon"`
+	Title string `json:"title"`
+}
+
+type RecommendProps struct {
+	Image       string `json:"image"`
+	ProductName string `json:"productName"`
+	Price       int    `json:"price"`
+	MallPrice   int    `json:"mallPrice"`
+}
+
+type SlidesFloorProps struct {
+	Id  int    `json:"id"`
+	Src string `json:"src"`
+}
+
+type FloorProps2 struct {
+	Title       string             `json:"title"`
+	FloorSlides []SlidesFloorProps `json:"floorslides"`
+}
+
+type FloorProps struct {
+	Floors []FloorProps2 `json:"floors"`
+
+	Slides          []SlidesProps    `json:"slides"`
+	Category        []CategoryProps  `json:"category"`
+	LeaderImage     string           `json:"leaderImage"`
+	LeaderUrl       string           `json:"leaderUrl"`
+	Recommend       []RecommendProps `json:"recommend"`
+	AdvertesPicture string           `json:"advertesPicture"`
+}
+
+type Floor struct {
+	Data    FloorProps `json:"data"`
+	Code    int        `json:"code"`
+	Message string     `json:"message"`
+}
+
+func Api_homePageContent(ctx iris.Context) {
+	slides := []SlidesProps{
+		{Id: 0, Image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBOBdFe_MJJj7v0fJr5tBiGOS64FfmrMkyEQ&usqp=CAU"},
+		{Id: 1, Image: "https://image.maigoo.com/upload/images/20221101/1404314044_750x498.jpg"},
+		{Id: 2, Image: "https://storage.googleapis.com/pos.shopline.hk/2021/11/onlineshop-vs-marketplace-hero-image.jpg"},
+	}
+
+	category := []CategoryProps{
+		{Id: 0, Icon: "https://m15.360buyimg.com/mobilecms/jfs/t1/187640/12/30456/5256/639c2315Ebc95c142/350a8f0c766f5460.png", Title: "京东超市"},
+		{Id: 1, Icon: "https://m15.360buyimg.com/mobilecms/jfs/t1/178015/31/13828/6862/60ec0c04Ee2fd63ac/ccf74d805a059a44.png", Title: "数码电器"},
+		{Id: 2, Icon: "https://m15.360buyimg.com/mobilecms/jfs/t1/54043/33/19389/4660/62b049dbE3b9aef75/2fcd31afd5d702e4.png", Title: "京东新百货"},
+		{Id: 3, Icon: "https://m15.360buyimg.com/mobilecms/jfs/t1/177902/16/13776/5658/60ec0e71E801087f2/a0d5a68bf1461e6d.png", Title: "京东生鲜"},
+		{Id: 4, Icon: "https://m15.360buyimg.com/mobilecms/jfs/t1/196472/7/12807/7127/60ec0ea3Efe11835b/37c65625d94cae75.png", Title: "京东到家"},
+		{Id: 5, Icon: "https://m15.360buyimg.com/mobilecms/jfs/t1/185733/21/13527/6648/60ec0f31E0fea3e0a/d86d463521140bb6.png", Title: "充值缴费"},
+		{Id: 6, Icon: "https://m15.360buyimg.com/mobilecms/jfs/t1/34248/39/16616/4689/62bbbdccE9f11132e/d51caf15f2f412b2.png", Title: "附近好店"},
+		{Id: 7, Icon: "https://m15.360buyimg.com/mobilecms/jfs/t1/37709/6/15279/6118/60ec1046E4b5592c6/a7d6b66354efb141.png", Title: "PLUS会员"},
+		{Id: 8, Icon: "https://m15.360buyimg.com/mobilecms/jfs/t1/191060/24/12861/6818/60ec11f2E67cf5ee6/c264378678b3cd96.png", Title: "京东国际"},
+		{Id: 9, Icon: "https://m15.360buyimg.com/mobilecms/jfs/t1/186882/8/12149/5894/60ec1250E9335241a/b22054613aa8ae75.png", Title: "京东拍卖"},
+	}
+
+	recommend := []RecommendProps{
+		{
+			Image:       "https://www.imuraya.co.jp/media_images/2019/03/box-azukiba-maccha.jpg",
+			ProductName: "商品名1",
+			Price:       20.0,
+			MallPrice:   30.0,
+		},
+		{
+			Image:       "https://www.calbee.co.jp/common/utility/binout.php?db=products&f=3152",
+			ProductName: "商品名2",
+			Price:       30.0,
+			MallPrice:   35.0,
+		},
+		{
+			Image:       "https://www.asahi-gf.co.jp/products/images/baby/milk/wakodo/gungun/40195890.jpg",
+			ProductName: "商品名3",
+			Price:       40.0,
+			MallPrice:   48.0,
+		},
+		{
+			Image:       "https://item-shopping.c.yimg.jp/i/l/pc-parts_tkm004",
+			ProductName: "商品名4",
+			Price:       60.0,
+			MallPrice:   78.0,
+		},
+	}
+
+	Floor1Props_Slides := []SlidesFloorProps{
+		{Id: 1, Src: "https://img.alicdn.com/bao/uploaded/i4/2200538036853/O1CN01LWLvBa20Uj5q1oL6O_!!0-item_pic.jpg"},
+		{Id: 2, Src: "https://img.alicdn.com/bao/uploaded/i1/2508879272/O1CN0150NbyE2IMdAzrAFXp_!!0-item_pic.jpg"},
+		{Id: 3, Src: "https://img.alicdn.com/bao/uploaded/i4/2508879272/O1CN01oGHDSp2IMd9gNIW0e_!!0-item_pic.jpg"},
+		{Id: 4, Src: "https://img.alicdn.com/bao/uploaded/i3/2508879272/O1CN01g1QHS82IMdAtk8umi_!!0-item_pic.jpg"},
+		{Id: 5, Src: "https://img.alicdn.com/bao/uploaded/i3/6000000001206/O1CN01lIkRv61KmOVSJjQjR_!!6000000001206-0-sm.jpg"},
+	}
+
+	res := Response{
+		Code:    200,
+		Message: "success",
+	}
+
+	FloorProps2Data := []FloorProps2{
+		{
+			Title:       "https://m.media-amazon.com/images/S/aplus-media-library-service-media/12d43eb0-bd46-4ede-8750-e69cc696ec45.__CR0,0,970,300_PT0_SX970_V1___.png",
+			FloorSlides: Floor1Props_Slides,
+		},
+	}
+
+	Floor1_Data := FloorProps{
+		Floors:          FloorProps2Data,
+		Slides:          slides,
+		Category:        category,
+		LeaderImage:     "https://file.adquan.com/upload/20230512/1683882554110940.jpg",
+		LeaderUrl:       "https://pub.dev/",
+		Recommend:       recommend,
+		AdvertesPicture: "https://pic40.photophoto.cn/20160910/0017029583521992_b.jpg",
+	}
+
+	data := Floor{
+		Data:    Floor1_Data,
+		Code:    res.Code,
+		Message: res.Message,
+	}
+	ctx.JSON(data)
+}
